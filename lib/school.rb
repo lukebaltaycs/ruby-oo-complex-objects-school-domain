c94b99d5b3e14e298f0a5b3d50b81d0ecb405d75
Luke Baltay <luke.baltay@yale.edu>
2022-05-31 14:23:27 -0400

    commit tree e5582bc0e517e7ca9af8f8c61f5dab0b79bc017c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+class School
+
+    def initialize(name)
+        @name=name
+        @roster={}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << student_name
+    end 
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        #sorted_roster = []
+        #@roster.each do |grade|
+        #    sorted_roster[grade] = grade.sort
+        #end
+        #sorted_roster
+        sorted_roster = @roster
+        sorted_roster.each do |grade, name|
+            sorted_roster[grade].sort!
+        end
+        return sorted_roster
+    end
+
+end
